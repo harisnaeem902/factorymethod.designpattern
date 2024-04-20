12aafadf8e56a5b5a3517b2c5d01ef1f49eb7e6c
Muhammad Haris Naeem <115229530+harisnaeem902@users.noreply.github.com>
2024-04-20 14:31:55 +0500

    first commit

---
+++ FactoryMethod.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.factorymethod;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author basitmalik
+ */
+public class FactoryMethod {
+
+    public static void main(String[] args) {
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+		
+		CircleGeometry cricleGeomerty = new CircleGeometry();
+		SquareGeometry squareGeometry = new SquareGeometry();
+                TriangleGeometry triangleGeometry = new TriangleGeometry();
+		
+		shapes.add(cricleGeomerty.createShape());
+		shapes.add(squareGeometry.createShape());
+                shapes.add(triangleGeometry.createShape());
+		
+		for(Shape s:shapes){
+			s.draw();
+		}
+	}
+
+}
---
+++ Triangle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.factorymethod;
+
+/**
+ *
+ * @author basitmalik
+ */
+public class Triangle implements Shape{
+
+	@Override
+	public void draw() {
+		System.out.println("Triangle is drawn");
+		
+	}
+	
+
+}
